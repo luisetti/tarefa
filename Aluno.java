8a3fd598409d2ed5266d1c94982043036d6446ba
luisetti <luis.s.silvafernando@gmail.com>
2023-05-03 17:21:35 +0000

    isaque

---
+++ Aluno.java
@@ -0,0 +1,26 @@
+public class Aluno extends Pessoa {
+    private String semestre;
+    private String curso;
+ 
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+    public String getSemestre() {
+        return semestre;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public String getCurso() {
+        return curso;
+    }
+
+    public Aluno(String nome, int idade, String endereco, String semestre, String curso){
+        super(nome, idade, endereco);
+        setSemestre(semestre);
+        setCurso(curso);
+    }
+    public Aluno(){
+
+    }
+}
